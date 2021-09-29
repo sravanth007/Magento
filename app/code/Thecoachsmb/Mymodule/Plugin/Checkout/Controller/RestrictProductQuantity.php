1d79b039e4624453a766220c0d103fe1b66cf71b
sravanth007 <sravanth.ranga@jivainfotech.com>
2021-09-29 17:18:01 +0530

    Sixth commit

---
+++ app/code/Thecoachsmb/Mymodule/Plugin/Checkout/Controller/RestrictProductQuantity.php
@@ -0,0 +1,14 @@
+<?php
+ 
+    namespace Thecoachsmb\Mymodule\Plugin\Checkout\Controller;
+ 
+    class RestrictProductQuantity
+    {
+        public function aroundExecute(
+            Index $subject,
+            \Closure $proceed
+        )
+        {
+            //You required logic
+        }
+    }
