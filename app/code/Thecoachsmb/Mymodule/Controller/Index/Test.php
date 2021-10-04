205000d9c6b2a5c5665e16aab5b3aeb3a413d18d
sravanth007 <sravanth.ranga@jivainfotech.com>
2021-10-04 17:31:32 +0530

    Eight Commit

---
+++ app/code/Thecoachsmb/Mymodule/Controller/Index/Test.php
@@ -0,0 +1,12 @@
+<?php
+namespace Thecoachsmb\Mymodule\Controller\Index;
+
+class Test extends \Magento\Framework\App\Action\Action
+{
+
+	public function execute()
+	{
+		echo "Hello World";
+		exit;
+	}
+}
