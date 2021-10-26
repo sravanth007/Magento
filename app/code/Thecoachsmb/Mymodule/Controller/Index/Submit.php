fbdd783f3cf2ad2d205c091333ca549f1b3db4ec
sravanth007 <sravanth.ranga@jivainfotech.com>
2021-10-26 17:59:58 +0530

    Eighteenth commit

--- app/code/Thecoachsmb/Mymodule/Controller/Index/Submit.php
+++ app/code/Thecoachsmb/Mymodule/Controller/Index/Submit.php
@@ -1,35 +1,35 @@
 <?php
- 
+
 namespace Thecoachsmb\Mymodule\Controller\Index;
- 
+
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
-use Meetanshi\Extension\Model\ExtensionFactory;
+
 use Magento\Framework\Controller\ResultFactory;
 use Magento\Framework\App\Action\Action;
- 
+
 class Submit extends Action
 {
     protected $resultPageFactory;
-    protected $extensionFactory;
- 
+
+
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory,
-        ExtensionFactory $extensionFactory
+        PageFactory $resultPageFactory
+
     )
     {
         $this->resultPageFactory = $resultPageFactory;
-        $this->extensionFactory = $extensionFactory;
+
         parent::__construct($context);
     }
- 
+
     public function execute()
     {
         try {
             $data = (array)$this->getRequest()->getPost();
             if ($data) {
-                $model = $this->extensionFactory->create();
+
                 $model->setData($data)->save();
                 $this->messageManager->addSuccessMessage(__("Data Saved Successfully."));
             }
@@ -39,6 +39,6 @@ class Submit extends Action
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
         $resultRedirect->setUrl($this->_redirect->getRefererUrl());
         return $resultRedirect;
- 
+
     }
 }
